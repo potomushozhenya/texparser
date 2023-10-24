c7d31d975a4e29cb1d265d6e39afe51e4faccc50
chmo_kudryavoe <avramenko.pav@yandex.ru>
2023-10-25 01:04:17 +0300

    fixes: "{...}", http x2 and bibio

--- libs_and_refs_pasha.py
+++ libs_and_refs_pasha.py
@@ -5,18 +5,23 @@ import re
 def parser():
     n=0
     pap=1
-    print("<div><strong>Литература</strong></div>")
+
     #  files/unzipped/2022-3-source/04/ref-s-eng.tex
     # files/unzipped/2018-1-source/06/lit-ra.tex
     # files/unzipped/2019-3-source/04/ref-s-eng.tex
     # files/unzipped/2022-1-source/07/ref-s-eng.tex
-    f = open('sources/2022-1-source/07/ref-s-eng.tex', 'r') #path c файлом
+    # sources/2020-2-source/09/ref-s.tex про кавычки
+    f = open('sources/2019-4-source/16/ref-s-eng.tex', 'r') #path c файлом
     worda=""
     wordb = ""
     wordc=""
     with f as myfile:
         lines = myfile.readlines()
-        while not f"{pap}. " in lines[n]:
+        while not f"{pap}. " in lines[n] or not f"{{pap}}":
+            if "References" in lines[n]:
+                print("<div><strong>References</strong></div>")
+            if "Литература" in lines[n]:
+                print("<div><strong>Литература</strong></div>")
             n += 1
 
         i=n
@@ -54,26 +59,56 @@ def parser():
             lines[i]=lines[i].replace("\linebreak\\newpage\\noindent","")# тэги для перехода на новую страницу в ""
 
             #курсив
-            lines[i]=lines[i].replace("\\textit{","<em>")
-            lines[i]=lines[i].replace("\\emph{","<em>")
-            lines[i] = lines[i].replace("{\it", "<em>")
-            lines[i] = lines[i].replace("{\itshape", "<em>")
-            lines[i]=lines[i].replace("}","</em>")
-            lines[i]=lines[i].replace("\\itshape","<em>")
-            lines[i] = lines[i].replace("\\upshape", "</em>")
+
+            if ("textit" in lines[i] or "emph" in lines[i] or "\\it" in lines[i] or "\\itshape" in lines[i]) and "{" in lines[i]:
+                if lines[i].find("{") < lines[i].find("textit") or lines[i].find("{") > lines[i].find("emph") or lines[i].find("{") > lines[i].find("\\it") or lines[i].find("{") > lines[i].find("\\itshape"):
+                    lines[i]=lines[i].replace("\\textit{","<em>")
+                    lines[i]=lines[i].replace("\\emph{","<em>")
+                    lines[i] = lines[i].replace("{\it", "<em>")
+                    lines[i] = lines[i].replace("{\itshape", "<em>")
+                    lines[i]=lines[i].replace("}","</em>")
+                    lines[i]=lines[i].replace("\\itshape","<em>")
+                    lines[i] = lines[i].replace("\\upshape", "</em>")
+                else:
+                    lines[i] = lines[i].replace("{", "")
+                    lines[i] = lines[i].replace("}", "")
+
+            elif "{" in lines[i]:
+                lines[i] = lines[i].replace("{","")
+                lines[i]= lines[i].replace("}","")
+            else:
+                lines[i] = lines[i].replace("\\textit{", "<em>")
+                lines[i] = lines[i].replace("\\emph{", "<em>")
+                lines[i] = lines[i].replace("{\it", "<em>")
+                lines[i] = lines[i].replace("{\itshape", "<em>")
+                lines[i] = lines[i].replace("}", "</em>")
+                lines[i] = lines[i].replace("\\itshape", "<em>")
+                lines[i] = lines[i].replace("\\upshape", "</em>")
+
 
             #спецсимволы
             lines[i] = lines[i].replace("---", "&mdash;")
-            lines[i]=lines[i].replace("~", "&nbsp;")
+            if lines[i].find("~")>0 and lines[i][lines[i].find("~")-1]!="\\":
+                lines[i]=lines[i].replace("~", "&nbsp;")
+            else:
+                lines[i]=lines[i][0:(lines[i].find("~")-1)]+lines[i][lines[i].find("~"):]
             lines[i]=lines[i].replace("--","&ndash;")
             lines[i]=lines[i].replace("\\-","")
 
             #http
             if lines[i].find("http")!=-1:
-                lines[i]=lines[i][0:lines[i].find("http")-1]+" <em>"+ lines[i][lines[i].find("http"):].replace(" ","</em> ")
-            if lines[i]!="\n":
+                if lines[i][len(lines[i])-1]!=" ":
+                    lines[i]+=" "
+                if lines[i].find("http")!=0:
+                    lines[i]=lines[i][0:lines[i].find("http")-1]+" <em>"+ lines[i][lines[i].find("http"):].replace(" ","</em> ")
+                else:
+                    lines[i] = lines[i][0:lines[i].find("http")] + " <em>" + lines[i][
+                                                                                 lines[i].find("http"):].replace(" ",
+                                                                                                                 "</em> ")
+
+            if lines[i]!="\n" and  not "%\bibitem"  in lines[i]:
                 # убираю лишние \ и $
-                lines[i]=lines[i].replace("\\","").replace("$","").replace("\\\\","")
+                lines[i]=lines[i].replace("\\\\","").replace("$","").replace("\\","",1000)
 
                 print("<div class=\"ref\">"+lines[i]+"</div>")
             i+=1
